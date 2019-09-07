3a417eb0a13e45594e454a113c49554924f8fbd7
Kristian Windsor <kristianwindso@gmail.com>
2019-09-07 04:51:36 -0700

    added phpmyadmin

--- db/src/createdatabase.sql
+++ db/src/createdatabase.sql
@@ -1,7 +1,6 @@
 
---
--- User: backend
---
-
 CREATE USER backend IDENTIFIED WITH mysql_native_password BY 'pass';
 GRANT ALL PRIVILEGES ON redditeye.* TO 'backend'@'%' WITH GRANT OPTION;
+
+CREATE USER phpmyadmin IDENTIFIED WITH mysql_native_password BY 'pass';
+GRANT ALL PRIVILEGES ON redditeye.* TO 'phpmyadmin'@'%' WITH GRANT OPTION;
