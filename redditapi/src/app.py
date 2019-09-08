93da0fb479b8e76dcebe9ad4c831da90fae68b5e
Kristian Windsor <kristianwindso@gmail.com>
2019-09-07 17:56:58 -0700

    integrated praw to redditapi

--- redditapi/src/app.py
+++ redditapi/src/app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python 
 
-import time
+import os, praw
 
-while True:
-    time.sleep(1)
+reddit = praw.Reddit(client_id=os.environ['CLIENT_ID'],
+                     client_secret=os.environ['CLIENT_SECRET'],
+                     user_agent=os.environ['USER_AGENT'])
+
+for comment in reddit.subreddit('all').stream.comments():
+    print(comment.body)
