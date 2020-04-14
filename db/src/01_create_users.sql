718e46794fbfffe27054a8bc67af3755adbe211f
KristianWindsor <kristianwindso@gmail.com>
2020-04-14 01:06:22 -0700

    deleted phpmyadmin

--- db/src/01_create_users.sql
+++ db/src/01_create_users.sql
@@ -2,8 +2,5 @@
 CREATE USER backend IDENTIFIED WITH mysql_native_password BY 'pass';
 GRANT ALL PRIVILEGES ON redditeye.* TO 'backend'@'%' WITH GRANT OPTION;
 
-CREATE USER phpmyadmin IDENTIFIED WITH mysql_native_password BY 'pass';
-GRANT ALL PRIVILEGES ON redditeye.* TO 'phpmyadmin'@'%' WITH GRANT OPTION;
-
 CREATE USER redditapi IDENTIFIED WITH mysql_native_password BY 'pass';
 GRANT ALL PRIVILEGES ON redditeye.* TO 'redditapi'@'%' WITH GRANT OPTION;
